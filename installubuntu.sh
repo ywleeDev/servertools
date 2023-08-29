0ca67a1f8b6df07eefd1fc4ba9214aaaf1ead25a
ywleeDev <yoonwoodev@gmail.com>
2023-08-29 16:07:05 +0900

    add sh

---
+++ installubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget  https://old-releases.ubuntu.com/releases/20.04.1/ubuntu-20.04-beta-desktop-amd64.iso
+wget  https://old-releases.ubuntu.com/releases/20.04.2/ubuntu-20.04-beta-desktop-amd64.iso
+wget  https://old-releases.ubuntu.com/releases/20.04.3/ubuntu-20.04-beta-desktop-amd64.iso
+wget  https://old-releases.ubuntu.com/releases/20.04.4/ubuntu-20.04-beta-desktop-amd64.iso
+wget  https://old-releases.ubuntu.com/releases/20.04.5/ubuntu-20.04-beta-desktop-amd64.iso
+wget  https://old-releases.ubuntu.com/releases/20.04.6/ubuntu-20.04-beta-desktop-amd64.iso
